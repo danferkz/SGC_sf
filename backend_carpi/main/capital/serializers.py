907bcb9a95e0312d5e580028a7b069899aaa0264
Johanna Antonella Vargas Flores <118397350+johannavargas5@users.noreply.github.com>
2024-09-16 20:18:32 -0500

    solucionado

--- backend_carpi/main/capital/serializers.py
+++
@@ -1,8 +0,0 @@
-# capital/serializers.py
-from rest_framework import serializers
-from .models import Capital
-
-class CapitalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Capital
-        fields = '__all__'
