5274a40f7400d9d2c656d81355e5c9e0dbece907
Johanna Antonella Vargas Flores <118397350+johannavargas5@users.noreply.github.com>
2024-09-22 20:39:27 -0500

    Tamaño y visualización

--- frontend_carpi/src/views/HomeAdminView.vue
+++ frontend_carpi/src/views/HomeAdminView.vue
@@ -25,17 +25,9 @@
         <li>
           <a href="#" class="flex items-center p-2 text-black rounded-lg hover:bg-melon-100">
             <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 00-2-2M5 11l7-7 7 7"></path>
-            </svg>
-            Productos
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center p-2 text-black rounded-lg hover:bg-melon-100">
-            <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v8m-4-4h8"></path>
             </svg>
-            Agregar Producto
+            Productos
           </a>
         </li>
         <li>
@@ -139,14 +131,14 @@
           </table>
         </section>
       </div>
-      <div class="w-120 ml-6">
+      <div class="w-120 ml-10">
         <h2 class="text-2xl font-bold text-black mb-2">Calendario</h2>
         <vue-cal
           class="mt-2"
           :events="events"
           :disable-times="true"
           :disable-day-numbers="true"
-          style="border: 1px solid #000; font-size: 14px; background-color: #f5f5dc; height: 380px;"
+          style="border: 1px solid #000; font-size: 14px; background-color: #f5f5dc; height: 400px; width: 400px;"
         />
       </div>
     </main>
