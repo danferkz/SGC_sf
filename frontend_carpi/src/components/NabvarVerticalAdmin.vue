83476cf499262333aa66c1e8a56627a148043703
Crralino <andresavelino.2003@gmail.com>
2024-10-18 11:27:57 -0500

    Creacion de la tabla de presupuesto, con filtros de pedidos realizados y fecha.

--- frontend_carpi/src/components/NabvarVerticalAdmin.vue
+++ frontend_carpi/src/components/NabvarVerticalAdmin.vue
@@ -130,7 +130,7 @@ const menuItems = ref([
     },
     {
         name: 'Presupuestos',
-        path: '/presupuestos',
+        path: '/gestionpresupuesto',
         icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 10c-4.41 0-8-1.79-8-4V8c0-2.21 3.59-4 8-4s8 1.79 8 4v6c0 2.21-3.59 4-8 4z" />
                </svg>`
