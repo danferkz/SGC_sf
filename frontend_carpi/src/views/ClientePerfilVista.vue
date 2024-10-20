89337e929b83d7c3f1954877a4f28d43e2398d8e
Johanna Antonella Vargas Flores <118397350+johannavargas5@users.noreply.github.com>
2024-10-20 17:38:30 -0500

    Cambios de dimensiones de cajas de texto en Puerta

--- frontend_carpi/src/views/ClientePerfilVista.vue
+++ frontend_carpi/src/views/ClientePerfilVista.vue
@@ -83,7 +83,7 @@
                 <div class="mt-6">
                     <button @click="openModal('update')"
                         class="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        actualizar Perfil
+                        Actualizar Perfil
                     </button>
                     <button @click="openModal('delete')"
                         class="ml-3 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
