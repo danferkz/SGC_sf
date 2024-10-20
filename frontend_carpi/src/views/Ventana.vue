4f39be4912072caebe5441b0125949f2a84fcf53
Johanna Antonella Vargas Flores <118397350+johannavargas5@users.noreply.github.com>
2024-10-20 17:42:21 -0500

    Cambios de cajas de ancho y alto en la vista Ventana

--- frontend_carpi/src/views/Ventana.vue
+++ frontend_carpi/src/views/Ventana.vue
@@ -24,12 +24,12 @@
           <div>
             <label for="height" class="block text-sm font-medium text-gray-700">Alto (cm)</label>
             <input type="number" id="height" v-model="formData.height" required min="50" max="300"
-              class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 focus:ring-amber-500 focus:border-amber-500 rounded-md">
+              class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 focus:ring-amber-500 focus:border-amber-500 rounded-md h-8">
           </div>
           <div>
             <label for="width" class="block text-sm font-medium text-gray-700">Ancho (cm)</label>
             <input type="number" id="width" v-model="formData.width" required min="50" max="300"
-              class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 focus:ring-amber-500 focus:border-amber-500 rounded-md">
+              class="mt-1 block w-full shadow-sm sm:text-sm border-gray-300 focus:ring-amber-500 focus:border-amber-500 rounded-md h-8">
           </div>
         </div>
 
