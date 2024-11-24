54d09a4d2de852082497bfc02750baa8ec7b8a1c
danferkz <gdanfer07@gmail.com>
2024-11-24 15:52:25 -0500

    actualizacion de estdos en vista de empleados

--- frontend_carpi/src/views/GestionEmpleados.vue
+++ frontend_carpi/src/views/GestionEmpleados.vue
@@ -30,6 +30,9 @@
                   <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Estado
                   </th>
+                  <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Acciones
+                  </th>
                 </tr>
               </thead>
               <tbody class="bg-white divide-y divide-gray-200">
@@ -46,17 +49,22 @@
                   <td class="px-6 py-4 whitespace-nowrap">
                     <span :class="[
                       'px-2 inline-flex text-xs leading-5 font-semibold rounded-full',
-                      empleado.is_active ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800',
+                      empleado.is_active ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
                     ]">
                       {{ empleado.is_active ? 'Activo' : 'Inactivo' }}
                     </span>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500">
+                    <button @click="editarEmpleado(empleado)" class="text-indigo-600 hover:text-indigo-900 mr-2">
+                      <PencilIcon class="h-5 w-5" />
+                    </button>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <!-- Nuevo Control de Paginación -->
+          <!-- Control de Paginación -->
           <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
             <div class="flex justify-center pb-4 space-x-2">
               <button @click="paginaAnterior" :disabled="!previousPage"
@@ -85,52 +93,30 @@
       </div>
     </div>
 
-    <!-- Modal Crear Staff -->
-    <div v-if="modalActivo === 'crearStaff'"
+    <!-- Modal Editar Empleado -->
+    <div v-if="modalActivo === 'editarEmpleado'"
       class="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
       <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 class="text-lg font-semibold mb-4">Crear Staff</h2>
-        <form @submit.prevent="crearStaff">
-          <div class="mb-4">
-            <label class="block text-gray-700">Username</label>
-            <input v-model="nuevoStaff.username" type="text" class="w-full border rounded-md px-4 py-2" required />
-          </div>
-          <div class="mb-4">
-            <label class="block text-gray-700">Email</label>
-            <input v-model="nuevoStaff.email" type="email" class="w-full border rounded-md px-4 py-2" required />
-          </div>
-          <div class="flex justify-end space-x-4">
-            <button @click="cerrarModal" class="bg-gray-200 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-300">
-              Cancelar
-            </button>
-            <button type="submit" class="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-              Continuar
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <!-- Modal Crear Empleado -->
-    <div v-if="modalActivo === 'crearEmpleado'"
-      class="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
-      <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 class="text-lg font-semibold mb-4">Crear Empleado</h2>
-        <form @submit.prevent="crearEmpleado">
-          <div class="mb-4">
-            <label class="block text-gray-700">Fecha de Contratación</label>
-            <input v-model="nuevoEmpleado.hire_date" type="date" class="w-full border rounded-md px-4 py-2" required />
-          </div>
+        <h2 class="text-lg font-semibold mb-4">Editar Empleado</h2>
+        <form @submit.prevent="actualizarEmpleado">
           <div class="mb-4">
             <label class="block text-gray-700">Especialidad</label>
-            <input v-model="nuevoEmpleado.specialty" type="text" class="w-full border rounded-md px-4 py-2" required />
+            <input v-model="empleadoEditado.specialty" type="text" class="w-full border rounded-md px-4 py-2"
+              required />
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700">Estado</label>
+            <select v-model="empleadoEditado.is_active" class="w-full border rounded-md px-4 py-2" required>
+              <option :value="true">Activo</option>
+              <option :value="false">Inactivo</option>
+            </select>
           </div>
           <div class="flex justify-end space-x-4">
             <button @click="cerrarModal" class="bg-gray-200 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-300">
               Cancelar
             </button>
             <button type="submit" class="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-              Crear Empleado
+              Actualizar
             </button>
           </div>
         </form>
@@ -142,18 +128,18 @@
 <script>
 import HeaderAdmin from "@/components/NabvarVerticalAdmin.vue";
 import axios from "axios";
+import { PencilIcon, TrashIcon } from 'lucide-vue-next'
 
 export default {
   components: {
     HeaderAdmin,
+    PencilIcon,
   },
   data() {
     return {
       modalActivo: null,
-      nuevoStaff: { username: "", email: "" },
-      nuevoEmpleado: {
-        user_id: null,
-        hire_date: "",
+      empleadoEditado: {
+        id: null,
         specialty: "",
         is_active: true,
       },
@@ -165,52 +151,7 @@ export default {
   },
   methods: {
     obtenerToken() {
-      return localStorage.getItem("token");
-    },
-    abrirModal(modal) {
-      this.modalActivo = modal;
-    },
-    cerrarModal() {
-      this.modalActivo = null;
-      this.resetForm();
-    },
-    resetForm() {
-      this.nuevoStaff = { username: "", email: "" };
-      this.nuevoEmpleado = {
-        user_id: null,
-        hire_date: "",
-        specialty: "",
-        is_active: true,
-      };
-    },
-    async crearStaff() {
-      try {
-        const response = await axios.post("http://127.0.0.1:8000/api/users/staff/create/", this.nuevoStaff, {
-          headers: {
-            Authorization: `Bearer ${this.obtenerToken()}`,
-          },
-        });
-        this.nuevoEmpleado.user_id = response.data.id; // Ajusta esto según la respuesta de la API
-        this.abrirModal('crearEmpleado'); // Abre el modal para crear el empleado
-      } catch (error) {
-        console.error("Error al crear staff:", error);
-        alert("Error al crear el staff. Por favor, inténtelo de nuevo.");
-      }
-    },
-    async crearEmpleado() {
-      try {
-        await axios.post("http://127.0.0.1:8000/api/employees/create/", this.nuevoEmpleado, {
-          headers: {
-            Authorization: `Bearer ${this.obtenerToken()}`,
-          },
-        });
-        alert("Empleado creado exitosamente.");
-        this.cerrarModal();
-        this.obtenerEmpleados(); // Refrescar la lista de empleados
-      } catch (error) {
-        console.error("Error al crear empleado:", error);
-        alert("Error al crear el empleado. Por favor, inténtelo de nuevo.");
-      }
+      return localStorage.getItem("token"); // Obtener el token desde localStorage
     },
     async obtenerEmpleados(url = "http://127.0.0.1:8000/api/employees/list/") {
       try {
@@ -221,17 +162,12 @@ export default {
           hire_date: empleado.hire_date,
           specialty: empleado.specialty,
           is_active: empleado.is_active,
+          employee_id: empleado.employee_id,  // Asegúrate de que 'id' sea el 'employee_id' o el nombre correcto en la respuesta
         }));
 
         this.nextPage = response.data.next;
         this.previousPage = response.data.previous;
-        if (this.nextPage) {
-          this.currentPage = this.extractPageNumber(this.nextPage) - 1;
-        } else if (this.previousPage) {
-          this.currentPage = this.extractPageNumber(this.previousPage) + 1;
-        } else {
-          this.currentPage = 1;
-        }
+        this.currentPage = this.extractPageNumber(this.nextPage) || this.extractPageNumber(this.previousPage) || 1;
       } catch (error) {
         console.error("Error al obtener empleados:", error);
         alert("Error al obtener la lista de empleados.");
@@ -248,9 +184,50 @@ export default {
       }
     },
     extractPageNumber(url) {
-      if (!url) return null;
-      const params = new URLSearchParams(url.split('?')[1]);
-      return parseInt(params.get('page')) || 1;
+      const match = url ? url.match(/page=(\d+)/) : null;
+      return match ? parseInt(match[1], 10) : null;
+    },
+    abrirModal(tipo) {
+      this.modalActivo = tipo;
+    },
+    editarEmpleado(empleado) {
+      this.modalActivo = 'editarEmpleado';
+      this.empleadoEditado = { ...empleado };
+    },
+    cerrarModal() {
+      this.modalActivo = null;
+      this.resetForm();
+    },
+    async actualizarEmpleado() {
+      try {
+        const response = await axios.put(
+          `http://127.0.0.1:8000/api/employees/update/${this.empleadoEditado.employee_id}/`,
+          {
+            specialty: this.empleadoEditado.specialty,
+            is_active: this.empleadoEditado.is_active,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${this.obtenerToken()}`, // Usamos el token de localStorage
+            },
+          }
+        );
+        alert("Empleado actualizado exitosamente.");
+        this.cerrarModal();
+        this.obtenerEmpleados(); // Refrescar la lista de empleados
+      } catch (error) {
+        console.error("Error al actualizar empleado:", error);
+        alert("Error al actualizar el empleado. Por favor, inténtelo de nuevo.");
+      }
+    },
+    resetForm() {
+      this.empleadoEditado = {
+        id: null,
+        username: "",
+        hire_date: "",
+        specialty: "",
+        is_active: true,
+      };
     },
   },
   created() {
@@ -258,3 +235,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/* Puedes agregar tus estilos aquí si es necesario */
+</style>
